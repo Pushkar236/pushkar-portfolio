1bdde1aa3fc45e0e1a4f19c04e4cb347837c92ba
Pushkar Kirange <pdkirange236@gmail.com>
2026-01-08 12:34:30 +0530

    changes in  tech section

--- src/components/sections/section-header.tsx
+++ src/components/sections/section-header.tsx
@@ -1,12 +1,21 @@
-import { cn } from "@/lib/utils"
-import Link from "next/link"
-import { BoxReveal } from "../reveal-animations"
-import { ReactNode } from "react"
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { BoxReveal } from "../reveal-animations";
+import { ReactNode } from "react";
 
-export const SectionHeader = ({ id, title, desc, className }: { id: string, title: string | ReactNode, desc?: string, className?: string }) => {
+export const SectionHeader = ({
+  id,
+  title,
+  desc,
+  className,
+}: {
+  id: string;
+  title: string | ReactNode;
+  desc?: string;
+  className?: string;
+}) => {
   return (
-
-    <div className={cn("top-[70px] sticky mb-96", className)}>
+    <div className={cn("top-[70px]  sticky mb-96", className)}>
       <Link href={`#${id}`}>
         <BoxReveal width="100%">
           <h2
@@ -23,5 +32,5 @@ export const SectionHeader = ({ id, title, desc, className }: { id: string, titl
         {desc}
       </p>
     </div>
-  )
-}
+  );
+};
